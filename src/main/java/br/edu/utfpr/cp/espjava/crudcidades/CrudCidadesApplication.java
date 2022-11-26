b0e2ed20d8244c5e74cd35c7d3ec3d2f412859cf
Gabriel Costa Silva <gabriel_costasilva@yahoo.com.br>
2022-11-26 14:56:45 -0300

    Updated Spring Boot to version 3

--- src/main/java/br/edu/utfpr/cp/espjava/crudcidades/CrudCidadesApplication.java
+++ src/main/java/br/edu/utfpr/cp/espjava/crudcidades/CrudCidadesApplication.java
@@ -1,13 +1,13 @@
 package br.edu.utfpr.cp.espjava.crudcidades;
 
-import javax.validation.Validator;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+
+import jakarta.validation.Validator;
 
 @SpringBootApplication
 public class CrudCidadesApplication {
--- src/main/java/br/edu/utfpr/cp/espjava/crudcidades/SecurityConfig.java
+++ src/main/java/br/edu/utfpr/cp/espjava/crudcidades/SecurityConfig.java
@@ -2,26 +2,30 @@ package br.edu.utfpr.cp.espjava.crudcidades;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @EnableWebSecurity
 @Configuration
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig {
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                    .withUser("john")
+    @Bean
+    public InMemoryUserDetailsManager configure() throws Exception {
+        UserDetails john = User.withUsername("john")
                     .password(cifrador().encode("test123"))
-                    .authorities("listar")
-                        .and()
-                    .withUser("anna")
+                    .roles("listar")
+                    .build();
+        
+        UserDetails anna = User.withUsername("anna")
                     .password(cifrador().encode("test123"))
-                    .authorities("admin");
+                    .roles("admin")
+                    .build();
+
+        return new InMemoryUserDetailsManager(john, anna);
     }
 
     @Bean
