c988ad1ec8869c7e1371204580f7ba7d32e18bcd
Gabriel Costa Silva <gabriel_costasilva@yahoo.com.br>
2022-11-26 13:16:00 -0300

    Updated Spring Boot to version 3

--- src/main/java/br/edu/utfpr/cp/espjava/crudcidades/visao/Cidade.java
+++ src/main/java/br/edu/utfpr/cp/espjava/crudcidades/visao/Cidade.java
@@ -1,7 +1,7 @@
 package br.edu.utfpr.cp.espjava.crudcidades.visao;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Size;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 public final class Cidade {
 
