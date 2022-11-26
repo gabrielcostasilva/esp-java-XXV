02b3795e501b8b2db3c1a634b0af20648551c49c
Gabriel Costa Silva <gabriel_costasilva@yahoo.com.br>
2022-11-26 13:24:37 -0300

    Updated Spring Boot to version 3

--- src/main/java/br/edu/utfpr/cp/espjava/crudcidades/visao/CidadeEntidade.java
+++ src/main/java/br/edu/utfpr/cp/espjava/crudcidades/visao/CidadeEntidade.java
@@ -2,10 +2,10 @@ package br.edu.utfpr.cp.espjava.crudcidades.visao;
 
 import java.io.Serializable;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity (name = "cidade")
 public class CidadeEntidade implements Serializable{
