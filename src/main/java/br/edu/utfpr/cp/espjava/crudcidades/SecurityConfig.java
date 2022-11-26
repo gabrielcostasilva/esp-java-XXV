6da124d6ef0f0f4b34ad3641ac24ab775c6ac450
Gabriel Costa Silva <gabriel_costasilva@yahoo.com.br>
2022-11-26 15:23:22 -0300

    Updated Spring Boot to version 3

--- src/main/java/br/edu/utfpr/cp/espjava/crudcidades/SecurityConfig.java
+++ src/main/java/br/edu/utfpr/cp/espjava/crudcidades/SecurityConfig.java
@@ -2,41 +2,47 @@ package br.edu.utfpr.cp.espjava.crudcidades;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
 @Configuration
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig {
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                    .withUser("john")
+    @Bean
+    public InMemoryUserDetailsManager configure() throws Exception {
+        UserDetails john = User.withUsername("john")
                     .password(cifrador().encode("test123"))
-                    .authorities("listar")
-                        .and()
-                    .withUser("anna")
+                    .roles("listar")
+                    .build();
+
+        UserDetails anna = User.withUsername("anna")
                     .password(cifrador().encode("test123"))
-                    .authorities("admin");
+                    .roles("admin")
+                    .build();
+
+        return new InMemoryUserDetailsManager(john, anna);
     }
 
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-            .csrf().disable()
-            .authorizeRequests()
-            .antMatchers("/").hasAnyAuthority("listar", "admin")
-			.antMatchers("/criar").hasAuthority("admin")
-			.antMatchers("/excluir").hasAuthority("admin")
-			.antMatchers("/preparaAlterar").hasAuthority("admin")
-			.antMatchers("/alterar").hasAuthority("admin")
-            .anyRequest().denyAll()
-            .and()
-            .formLogin().permitAll();
+    @Bean
+    public SecurityFilterChain filter(HttpSecurity http) throws Exception {
+        return http
+                .csrf().disable()
+                .authorizeHttpRequests()
+                .requestMatchers("/").hasAnyRole("listar", "admin")
+                .requestMatchers("/criar", "/excluir", "/alterar", "/preparaAlterar").hasRole("admin")
+                .anyRequest().denyAll()
+                .and()
+                .formLogin().permitAll()
+                .and()
+                .build();
+
     }
 
     @Bean
